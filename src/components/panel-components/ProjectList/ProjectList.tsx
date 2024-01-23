06948e94b89e62657adf0f45cdb4883676747d00
Austin Chiatto <chiattoaustin@gmail.com>
2024-01-22 17:55:00 -0800

    update for type error

--- src/components/panel-components/ProjectList/ProjectList.tsx
+++ src/components/panel-components/ProjectList/ProjectList.tsx
@@ -2,21 +2,21 @@ import ProjectSummaryCard from './ProjectSummaryCard/ProjectSummaryCard';
 import { PanelProps } from '@/data/panel-data';
 
 const ProjectList = ({ panelContent }: { panelContent: PanelProps }) => {
-    const panelChildrenArray: PanelProps[] = panelContent.childPanels ?? [];
-    return (
-        <>
-            {panelChildrenArray.map((e, i) => (
-                <ProjectSummaryCard
-                    key={i}
-                    title={e.intro.title}
-                    desc={e.panelComponentProps.cardDesc}
-                    image={e.panelComponentProps.image}
-                    cardData={e}
-                    index={i}
-                />
-            ))}
-        </>
-    );
+  const panelChildrenArray: PanelProps[] = panelContent.childPanels ?? [];
+  return (
+    <>
+      {panelChildrenArray.map((e, i) => (
+        <ProjectSummaryCard
+          key={i}
+          title={e.intro.title}
+          desc={e.panelComponentProps?.cardDesc ?? ''}
+          image={e.panelComponentProps?.image ?? ''}
+          cardData={e}
+          index={i}
+        />
+      ))}
+    </>
+  );
 };
 
 export default ProjectList;
