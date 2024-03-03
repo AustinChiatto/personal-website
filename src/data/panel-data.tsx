b00e751fdcf608de7feaa46e6703dec58bfd66fe
Austin Chiatto <chiattoaustin@gmail.com>
2024-03-02 22:29:11 -0800

    feat: add shallot project skeleton to help visualize non-featured project cards

--- src/data/panel-data.tsx
+++ src/data/panel-data.tsx
@@ -2,9 +2,10 @@ import { ListCardProps } from '@/components/ListCard/ListCard';
 import { componentMap } from './component-map';
 // project-data imports
 import { MarketMaestroData } from './project-data/market-maestro-data';
+import { Code2ImageData } from './project-data/code2image-data';
 import { AsteroidRunData } from './project-data/asteroid-run-data';
 import { GridToolData } from './project-data/grid-builder-data';
-import { Code2ImageData } from './project-data/code2image-data';
+import { ShallotData } from './project-data/shallot-data';
 import { ArticleProps, ExternalLinkProps, TechStackProps } from './project-data/types';
 
 type IntroProps = {
@@ -85,7 +86,7 @@ export const panelData = {
           panelComponentProps: {
             image: '/images/market-maestro_work-thumbnail.webp',
             imageBase64:
-              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAKCAIAAAA2KZn2AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAEqADAAQAAAABAAAACgAAAADKOW4WAAABvklEQVQoFU1Qy24TQRDsnp3ZXXstP3gJJQgkDjwkpOQL+A++gQ/ggHKAAxJ/gQTikDMXEEhcckHhGBEpQQk4xsHx2s6+Zmd2todZ2wfq1Oru6uoqnMgFANVg81qdVufHdDak6QxSDSVY1sbgOl65w27c47c2vKstxhHAYz53HEnVx69fXj1/TYOuOB1ZN2lAAGxdBT6zAFrv7n/eCLsciOVG78fDvb8/WZL+x3H7a05TKe04rvgwPviRXFRk+ffp+bfJeJ4a113p3PbCx8FAWNpkISK+zH5jo9xgNCv25J8ORPxwlC5Kkxm5GhRbDw+VOej3+WLOyH0GgHeJbOvoxL0dKxloddLO2Cwzk7ws9dqQP5667XD4i+WS5TkJbjodEcdLq5BJupBFklc8ThXWfkhilYGYzlkhAWngBQVYnhVYVeA6S7RNpJWXdGsG2gdmleccNxlYMpimLC02NUVS4mW25DTe0IIUmQDutrm1VoHZvv/o6YsHYD2G5IIZHh2/f/tOeF4P6dnODiHUdQ1kdutWiZVlFp+8+WQD0+sDdPMsmunOpeZKgbLueKOOPvBQRyLvtZIeJFGasK1rN/8BPJj3hRIYC3cAAAAASUVORK5CYII=',
+              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAACqADAAQAAAABAAAACAAAAABBuOuVAAAA8ElEQVQYGXWPy0rEQBBFT1d6knQmPpiVKD6XKvhp/oHowv9zNgpRUNw4g6PjvEjS6SR2xtl6obgFdYpbpa7u39redg19R2UcyW6DMxWS1pitloUpcIlDUwSkqXCyE1FoD0tFI5qBFoSaTxfwmi89mAdMvx0zazk/1USRRpoAVzoelitInU8TdDvPYfFMpUuGY3+C8RVtvOvjBrk4RDeTF4rHa+rS8p/6tzc+uvSXeEjimEvpcRQlGCUM8zlZ7qM7ecaDwbpPzo7JpjPe9/ewPzP0wYDwY4SdfG1Ap+6UUqyesr/l0XjtEoY0/sFuhhV+AZF6ZjxYMTuVAAAAAElFTkSuQmCC',
             cardDesc:
               'Responsive Next.js site that I built as part of my internship with Dovetail Digital.',
             externalLinks: MarketMaestroData.externalLinks,
@@ -145,7 +146,7 @@ export const panelData = {
           panelComponentProps: {
             image: '/images/code2image_project-thumbnail.webp',
             imageBase64:
-              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAKCAIAAAA2KZn2AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAEqADAAQAAAABAAAACgAAAADKOW4WAAABxklEQVQoFSVQu25TQRCdnd31ffiVGGInKEZIoUHIogIhBBRIfACi5hdCS5OKgpKOmpZ/gIYKRQniJSHoLJIAUcAvfOO9np1hLoy0K83umTPnHHP19hfr2CGjEVmGuJhSeRIWhyV9X9JJpCnFeYxF5EJiSXGxZNrsd52wjdE2cvN4p2ONsJRAJUkpihMBE4GFIe7u755ptx5u77BQXC7M9bvfnMONdXz2tP1nctxb61qdNrob/pcxKkMbBsDO6hYz9c+fc0meuRTSFWxldnRMiAhgXn6GJ698nkNWh6QJWVMeXeGtJnPUz4rNYe59HX1HN8D6Zl+Elf7Xwgz6dH9AtbTma/bFMMzFKlog6KnGTOZsG01HpYA3YCzqu/Ow0oC1NNQbKcOynRaIjQqu5plEopPUUgOpxc/fFbeOPg6vXbhztpd43BvJ0Sfw2SjJ4KeVe07FCcegPoXJUc1CAno/MF9HFy/fLMZKeqMfL/UQTN26CMagla4qAU1VWzUPLhgQiwH4w8aAuUBpmh/jKj1bRc1VSZyfHp5OJZSA/xyqyCgmMBzM7PbrAD5gSi7nNBHnK7dM5XwymwwPxm/3Zu/f2NUOjX4r6V+oi/AKr3gGcwAAAABJRU5ErkJggg==',
+              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAACqADAAQAAAABAAAACAAAAABBuOuVAAABCElEQVQYGV2QS0rDUBSGv9xH0puk0QSrUhDBF+gahM5cjLtw0EU4cQW6BHcgCE46cODEVhQVqkma5nW9Y8/k8B8+fj6Od31+Z0P5SzKsUatPYlMxsD8YuyROIcgXGJGj1tagtcWGLfFBim9zNAUUH5BK1s9LhO0c2IeIVYefg/pqUVXFMLF43w22LPC7AQJ3t9GQ0UlEIFZIr8QXAQi3M4NwWYkRzcsMlR1lXExTwiji/8znr9R1g765RbXWVStnVRQ8LCKm93C8JzExdGafy9MOHOMcA7SSKKWQGiaHJZPxG9nWmMfco+x99wEHtlZfzZ7e2TxLEDJkIxbsbu+gQufdV3iixzH8AUmFX3S9RZUkAAAAAElFTkSuQmCC',
             cardDesc: 'Built to learn the basics of React.',
             externalLinks: Code2ImageData.externalLinks,
             listCards: [
@@ -186,7 +187,7 @@ export const panelData = {
           panelComponentProps: {
             image: '/images/asteroid-run_project-thumbnail.webp',
             imageBase64:
-              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAKCAIAAAA2KZn2AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAEqADAAQAAAABAAAACgAAAADKOW4WAAABhElEQVQoFT1R247UMAy1HbtN06aXmZKd1QDahWUREuKBhxUv/P9nrITEByBEp9NLgksBP1iyc3yOj4MhBABAxKQBhBD3UjuAhoiB9qxFjoaZ6MvTEyd9TbCu6XK56KCiMynneRTJjOA0/aqc70P9/PzNcPb48FlMdqxvmABjiinFZd50tojLsi5Kxatcr5NE8/06juPgbLo/vHaS37UnVgUiiv9GABKhdlBZIsxKcl3WafypWyjsY3+qcnPX1fzXgzF/hPakUwiKIpPmRdU3DIBF+nTy3tk+NIybaUHSsR+7N0XENfmmHYbB5EiJYb3q1UTk4W1wtS1f9gycce5jVL6dFAmZWMbLpPuq5QjqXE8FmHF4/4p9Lt2BOa9zd4A1qat9xZiWeZ0US6j2/vOhMcbdn9kWWFi2vi/cUSKM3RnTIgiWuKhuy7LMOA7TnEBmmIX5zeM76TqyTn+QffmiqcOtrb+ePwSeTqW56Wzbt2Vos2OTtZ4qbwqbF645HoyrtpUQfwOUU4v6i3SO+gAAAABJRU5ErkJggg==',
+              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAACqADAAQAAAABAAAACAAAAABBuOuVAAAA7UlEQVQYGSWQTU7DMBCFP/8kJU2FWEAFYk8FCxZsOQQ34Q5cpitu03UXVEJCFWoFTUNiOzbj9C3smTd+o/esXp/bRK1wGqig8aBr6BUcpM78j0voIsnAJxb3inoCt9eKoYVZCU8PCrkoIth8pB6268TFHMwMlJHHFj4/Er5JWNlqh/DLV7cSAvwGBmNwDHgZRm3pVODs/BG7O654X79Q6ikutlTVlElhadqem8srdoc9d4sl1ojHEWJeKY1znu6vJdPb7z19OGJzDiseR4yCRIyBopRUAiXiDJPDiJW3sZMihdP6EORfBDGdeiuCf0xbXpa5Y9CEAAAAAElFTkSuQmCC',
             cardDesc: 'Created to teach myself JavaScript object-oriented programming.',
             externalLinks: AsteroidRunData.externalLinks,
             listCards: [
@@ -227,8 +228,8 @@ export const panelData = {
           panelComponentProps: {
             image: '/images/grid-tool_project-thumbnail.webp',
             imageBase64:
-              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAKCAIAAAA2KZn2AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAEqADAAQAAAABAAAACgAAAADKOW4WAAABk0lEQVQoFT1QzW4TMRAej+3dZLspVapIbRWJwgFx5dZH4aXgcbhz6yEXJCQQET+CCFqilmSb2uv12NPZjcrIsuzRfD/zqW/LjWJgTt7l9Sr//MFXv7jZqi6AglxWanrM86dqfq6nMyxKrRQXlVHfv2xypBR5/uLA2FJGQWlmhqHkgUqtVqvFYnF6dnaYnk9mdlQbtfywXi+3vkkXr58RU6lNyoAISmgfwaonSoj47s3H81fTk5dH2Hz913y+0sFJtw0kKgKQErH9DYAAWZDSwqa5+fTbXe8wXv81OcTInThNKqUkACLKOYtazr3X/Vf+5KMNPt7eIlC630RuO2MlGpFEmeYeC4Icak+RpN3tPN1HnRKSa8cYqwpgGB0MiiMxJib1YE8J17AeP5mIdJdawnJszIH1QTsCY2wXQp+dCP9fSbbqD2vEHRXFYWGsRGasa4SUJqWuNNlipIEHegaNWuZRMnzMqSPvGIzE7WNdx+zuLt++1/W4PDkaHVdlXaBBiS/H3DW+/XMXbrbs4uxUIpZWfAATe/NUxwUmEQAAAABJRU5ErkJggg==',
-            cardDesc: 'A CSS grid tool built to learn React.',
+              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAACqADAAQAAAABAAAACgAAAAA7eLj1AAABGklEQVQYGT2Qu0oDQRiFv52dJAREDai9lY34BnbWNr6Dj5HSt0lt50vYCYaAJooRFUzc7M7l9+xGHJhhznAu/5zienIfQki+CjWrTcXX5oeXas2sXpOaCmINFqKfzxc+xEgVG1YxsJTgNTekHIAMgxJ86f3j/Imbq3OSGbVlbcMKEQpYvr8RcmT8MMU/LJ65OD2mEMl7pxiTk3iFmJyIb4ynM5wtlx343gRyFmnLI6UknGmfcAXePj86dSnQqltDa+P/neVuCV/opyFZ59aGmY5W0q5tvO5JY5Eik9s7sqJcWWKl5vQe+j39uIcbDpChpGeXRqOuspBTFYM+7AxhfxcO9uBwBEcjHM5FZXRRfwN29bVxSKveVDrxF4SMm0shJXLmAAAAAElFTkSuQmCC',
+            cardDesc: 'CSS grid tool that I built to learn React.',
             externalLinks: GridToolData.externalLinks,
             listCards: [
               {
@@ -252,6 +253,47 @@ export const panelData = {
               panelComponent: 'ProjectTechStack',
               panelComponentProps: {
                 techStack: GridToolData.techStack
+              }
+            }
+          ]
+        },
+        // shallot
+        {
+          id: 'Shallot',
+          level: 3,
+          intro: {
+            title: 'Shallot',
+            desc: 'Retro handheld theme that I designed for fun.'
+          },
+          panelComponent: 'ProjectDetails',
+          panelComponentProps: {
+            image: '/images/shallot_project-thumbnail.webp',
+            imageBase64:
+              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAACqADAAQAAAABAAAACgAAAAA7eLj1AAABS0lEQVQYGT2QzU4UQRSFv5q+VdXT0wPoRIEQdONbQHBnfAd3vgcP4JPwAixYiSxIeAHjho1xoRFmtGf6r7qrq6nZcJK7OfnuPblHfbq46xcrJXHYKxSzFhKlwELIAsN0oE8HL94F8Y2iXhUsf95jnWJClIHF62NeHR8hCSJj56FXfP9xzeXVly3yrM8fzzl485ZkS7pNgasS+s6RWcvp+xOMnXL99Rvj9vRkRCUKqVcPlGuhdTW1c9zc3GKMoaoq+tDR9g26D8jm8Q+T2rDzcpezkw/4YWBUgTzJ2D88YtOsMaVBytVfVGd492JBuRHmewfkY46ue3Q2p1gvsWoawWJJ8EKVGmb5PlZHczrDS0sd45uhJh0apCn/Mwbh9z8hDTYu6VigEJzl8WFJqjXed/HrtvJjSORX67AR0EqTTgy5mbFjPfM0Y/SDfwI7+JQrfFtk0AAAAABJRU5ErkJggg==',
+            cardDesc: 'Retro handheld theme that I designed for fun.',
+            externalLinks: ShallotData.externalLinks,
+            listCards: [
+              {
+                cardTitle: 'Tech Stack',
+                cardType: 'list',
+                buttonLabel: 'All Tools',
+                buttonChildIdRef: 'projectTechStack',
+                cardContents: ShallotData.techStack
+              }
+            ],
+            article: ShallotData.article
+          },
+          childPanels: [
+            {
+              id: 'projectTechStack',
+              level: 4,
+              intro: {
+                title: 'Project Stack',
+                desc: 'The Languages and Tools used in the development of this project.'
+              },
+              panelComponent: 'ProjectTechStack',
+              panelComponentProps: {
+                techStack: ShallotData.techStack
               }
             }
           ]
---
+++ src/data/project-data/shallot-data.ts
@@ -0,0 +1,42 @@
+const externalLinks = [
+  {
+    label: 'View Theme',
+    href: 'https://github.com/OnionUI/Themes/blob/main/README.md',
+    favicon: '/icons/github-original.svg'
+  }
+];
+
+const techStack = [
+  {
+    label: 'Figma',
+    icon: '/icons/figma-original.svg',
+    desc: 'Utilized for game controls and function'
+  },
+  {
+    label: 'Aseprite',
+    icon: '/icons/html5-original.svg',
+    desc: 'Selected as the renderer for game assets'
+  },
+  {
+    label: 'JSON',
+    icon: '/icons/css3-original.svg',
+    desc: 'Chosen for its simplicity.'
+  },
+  {
+    label: 'OnionOS',
+    icon: '/icons/vscode-original.svg',
+    desc: '[PH]'
+  }
+];
+
+const article = [
+  {
+    markdownSection: ['']
+  }
+];
+
+export const ShallotData = {
+  externalLinks,
+  techStack,
+  article
+};
